528cd7344fe7f4e6fc8112e1a80705257452db5d
Matt Leick <mattleick@Matt-Leicks-MacBook-Pro.local>
2018-11-17 19:43:20 -0600

    First Commit
    Basic Webpack setup with React and hot reloading

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+class App extends Component {
+    render () {
+        return (
+        <h1>Howdy!</h1>
+        );
+    }
+}
+
+export default hot(module)(App);
